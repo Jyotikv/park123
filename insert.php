689ec66dead481bf0722f74fbfabb80af017ed5e
park w <demoparking2018@gmail.com>
2017-04-23 08:28:08 -0500

    done

--- insert.php
+++ insert.php
@@ -10,7 +10,7 @@
      //connect to the db
     $con=mysql_connect('us-cdbr-iron-east-03.cleardb.net','bb8ff899f74f6a','ff6ca091')  or die ("Con Error".mysql_error());
     mysql_select_db('ad_254e48c6f6af81f',$con);
-     $sql = "Insert into studentinfo2 values('$usn','$name')";
+     $sql = "insert into studentinfo2(USN,Name) values('$usn','$name')";
      echo $sql;
      // the result of the query
      $result = mysql_query($sql,$con) or die("Invalid query: " . mysql_error());
