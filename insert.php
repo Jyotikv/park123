1fd3ce9e801964246860c546879ad43d72e0576d
park w <demoparking2018@gmail.com>
2017-04-23 08:44:21 -0500

    one

--- insert.php
+++ insert.php
@@ -20,17 +20,17 @@ else
 {
     mysql_select_db("ad_254e48c6f6af81f");
     
-    $query="insert into studentinfo2 values($usn,$name)";
+    $query="insert into studentinfo2 values('$usn','$name')";
     
     $result=mysql_query($query,$connection);
     
     if(!$result)
     {
-        echo "insertion failed";
+     echo "insertion failed";
     }
     else
     {
-        echo "inserted successfully";
-    }
+     echo "inserted successfully";
+  	 }
     mysql_close($connection);
 }
